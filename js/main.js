4bb5b304a2bce99f404cdeeabb323732056b77e4
ahmedsalama6621 <a.salama6621@gmail.com>
2024-07-23 15:48:49 +0300

    System Login

---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+if (window.location.pathname === "/signup.html" || window.location.pathname === '/System-Login/') {
+    let btn = document.getElementById("signup");
+    btn.addEventListener("click", function (e) {
+        let name = document.getElementById("name");
+        let email = document.getElementById("email");
+        let password = document.getElementById("password");
+        let gender = document.getElementById("gender");
+        e.preventDefault();
+        if (name.value === "") {
+            swal("Name is Requerd", "This Name Will Be Your Nick Name");
+        } else {
+            window.localStorage.setItem("name", name.value);
+        }
+        if (email.value === "") {
+            swal("Email is Requerd", "Enter A Valid Email");
+        } else if (!email.value.includes("@")) {
+            swal("email Shoud Include '@' And '.'");
+        } else {
+            window.localStorage.setItem("email", email.value);
+        }
+        if (password.value === "") {
+            swal("Password is Requerd");
+        } else if (password.value.length < 8) {
+            swal("password Must Be 8 Number Or More ");
+        } else {
+            window.localStorage.setItem("password", password.value);
+        }
+        if (gender.value === "male") {
+            window.localStorage.setItem("gender", gender.value);
+        }
+        if (gender.value === "female") {
+            window.localStorage.setItem("gender", gender.value);
+        }
+        name.value = ""
+        email.value = ""
+        password.value = ""
+    });
+}
+if (window.location.pathname === "/index.html" || window.location.pathname === '/System-Login/') {
+
+    let signIn = document.getElementById("signin")
+    signIn.addEventListener("click", function (e) {
+        e.preventDefault();
+        let user = document.getElementById("user")
+        let userStorage = localStorage.getItem("email")
+        let pass = document.getElementById("pass")
+        let passStorage = localStorage.getItem("password")
+        if (user.value === userStorage) {
+            window.location.href = 'home.html';
+        } else {
+            swal("Email is not available")
+        }
+        if (pass.value === passStorage) {
+            window.location.href = 'home.html';
+        } else {
+            swal("The password is incorrect")
+        }
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname === "/home.html" || window.location.pathname === '/System-Login/') {
+        let username = localStorage.getItem('name');
+        let gender = localStorage.getItem('gender');
+        let welcomeMessage = document.getElementById('welcomeMessage');
+
+        if (username && gender) {
+            let title = gender === 'male' ? 'Mr. ' : 'Miss ';
+            welcomeMessage.textContent = 'Hello ' + title + username;
+        }
+    }
+}); document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname === "/home.html") {
+        let username = localStorage.getItem('name');
+        let gender = localStorage.getItem('gender');
+        let welcomeMessage = document.getElementById('welcomeMessage');
+        if (username && gender) {
+            let title = gender === 'male' ? 'Mr. ' : 'Miss ';
+            welcomeMessage.textContent = 'Hello ' + title + username;
+        }
+
+    }
+});
